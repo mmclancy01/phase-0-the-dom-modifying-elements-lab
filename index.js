2543a70f9de8308dfe9f8d69ade1f88177ec073c
Matthew Clancy <mmclancy01@yahoo.com>
2024-01-24 18:13:27 -0500

    dom

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const mainElement = document.getElementById('main');
+if (mainElement) {
+    mainElement.parentNode.removeChild(mainElement);
+  }
+  const newHeader = document.createElement('h1');
+  newHeader.id = 'victory';
+  newHeader.textContent = 'YOUR-NAME is the champion';
+  document.body.appendChild(newHeader);
+
+  
